886a16cc751f7c7ec6f213aef1a31bd3d7402940
Ian Cossentino <icossentino@gmail.com>
2021-01-06 11:32:03 -0500

    refactor auth controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,30 +3,36 @@
 class AuthenticationController < ApplicationController
 
     get '/authentication/login' do
-        erb :'/authentication/login', :layout => :'../views/authentication/layout'
+        erb :'/users/authentication/login', :layout => :'../views/users/layout'
+    end
+
+    get '/authentication/failure' do
+        erb :'/users/authentication/failure', :layout => :'../views/users/layout'
     end
 
     get '/registration/signup' do
-        erb :'/authentication/signup', :layout => :'../views/authentication/layout'
+        erb :'/users/registration/signup', :layout => :'../views/authentication/layout'
     end
 
     post '/authentication' do
-        @user = User.find_by(username: params[:username], password: params[:password])
-        if @user
+        @user = User.find_by(username: params[:username])
+        if @user && @user.authenticate(params[:password])
           session[:user_id] = @user.id
           redirect '/recipes'
+        else
+          redirect '/authentication/failure'
         end
-        redirect '/sessions/login'
     end
+
 
     post '/registration' do
         @user = User.find_by(username: params[:username])
         if !@user
-            new_user = User.new(username: params[:username], password: params[:password])
-            session[:user_id] = @user.id
+            new_user = User.create(username: params[:username], password: params[:password])
+            session[:user_id] = new_user.id
             redirect '/recipes'
         end
-        redirect '/sessions/login'
+        redirect '/registration/failure'
     end
 
 
