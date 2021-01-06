a2afd61ff5408e02c96ffb7cad370333fa43889c
Ian Cossentino <icossentino@gmail.com>
2021-01-06 11:20:34 -0500

    writing auth controller

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -6,8 +6,27 @@ class AuthenticationController < ApplicationController
         erb :'/authentication/login', :layout => :'../views/authentication/layout'
     end
 
-    get '/authentication/signup' do
+    get '/registration/signup' do
         erb :'/authentication/signup', :layout => :'../views/authentication/layout'
+    end
+
+    post '/authentication' do
+        @user = User.find_by(username: params[:username], password: params[:password])
+        if @user
+          session[:user_id] = @user.id
+          redirect '/recipes'
+        end
+        redirect '/sessions/login'
+    end
+
+    post '/registration' do
+        @user = User.find_by(username: params[:username])
+        if !@user
+            new_user = User.new(username: params[:username], password: params[:password])
+            session[:user_id] = @user.id
+            redirect '/recipes'
+        end
+        redirect '/sessions/login'
     end
 
 
